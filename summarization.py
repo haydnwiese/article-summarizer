b0a1018f215d1ed8fa45d24d292bbef8fea3e7f7
haydnwiese <45501692+haydnwiese@users.noreply.github.com>
2020-05-11 14:43:46 -0400

    Added functions to calcualte score of each sentence and average sentence score

--- summarization.py
+++ summarization.py
@@ -22,6 +22,35 @@ def create_frequency_table(text_string) -> dict:
     return freq_table
 
 
+def score_sentences(sentences: list, freq_table: dict) -> dict:
+    sentence_value = dict()
+
+    for sentence in sentences:
+        word_count_in_sentence = len(word_tokenize(sentence))
+        for word_value in freq_table:
+            if word_value in sentence.lower():
+                if sentence[:10] in sentence_value:
+                    sentence_value[sentence[:10]] += freq_table[word_value]
+                else:
+                    sentence_value[sentence[:10]] = freq_table[word_value]
+
+        # Divide by number of words in sentence to ensure longer sentences do not have advantage
+        sentence_value[sentence[:10]] = sentence_value[sentence[:10]] // word_count_in_sentence
+
+    return sentence_value
+
+
+def find_average_score(sentence_value: dict) -> int:
+    sum_values = 0
+    for value in sentence_value.values():
+        sum_values += value
+
+    # Average value of a sentence from original text
+    average = int(sum_values / len(sentence_value))
+
+    return average
+
+
 def generate_summary(text) -> str:
     # Create word frequency table
     freq_table = create_frequency_table(text)
