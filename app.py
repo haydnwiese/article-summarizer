f9cad0512f70f26ce142eb608685fad2e6ce7be2
haydnwiese <45501692+haydnwiese@users.noreply.github.com>
2020-05-13 14:15:06 -0400

    Updated app and service

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from service import create_summarization
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def menu():
+    return render_template('menu.html')
+
+
+@app.route('/summary', methods=["POST"])
+def summary():
+    article_url = request.form['article-url']
+    article_summary = create_summarization(article_url)
+    if article_summary:
+        return render_template('summary.html', summary=article_summary)
+    else:
+        return "Unsuccessful"
+
+
+if __name__ == '__main__':
+    app.run()
--- service.py
+++ service.py
@@ -3,22 +3,24 @@ import urllib.request as request
 import re
 from summarization import run_summarization
 
-scraped_data = request.urlopen('https://en.wikipedia.org/wiki/Artificial_intelligence')
-article = scraped_data.read()
 
-parsed_article = BeautifulSoup(article, 'lxml')
-paragraphs = parsed_article.find_all('p')
+def create_summarization(article_url: str) -> str:
+    scraped_data = request.urlopen(article_url)
+    article = scraped_data.read()
 
-article_text = ""
-for para in paragraphs:
-    article_text += para.getText()
+    parsed_article = BeautifulSoup(article, 'lxml')
+    paragraphs = parsed_article.find_all('p')
 
-# Removing Square Brackets and Extra Spaces
-article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
-article_text = re.sub(r'\s+', ' ', article_text)
+    article_text = ""
+    for para in paragraphs:
+        article_text += para.getText()
 
-# Removing special characters and digits
-formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
-formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+    # Removing Square Brackets and Extra Spaces
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+    article_text = re.sub(r'\s+', ' ', article_text)
 
-print(run_summarization(article_text))
+    # Removing special characters and digits
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+
+    return run_summarization(article_text)
